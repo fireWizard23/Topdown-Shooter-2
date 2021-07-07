c436626d7c992ae334a56c46ae925618dbc0296c
fireWizard23 <62689803+fireWizard23@users.noreply.github.com>
2021-07-07 19:21:07 +0800

    Commit 0.13 - Player States
    Added player state to the playercontroller class and made the rigidbody.velocity to zero if it;s less than 0.1

--- project/Topdown Shooter ver 2/Assets/Scripts/Player/PlayerController.cs
+++ project/Topdown Shooter ver 2/Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [RequireComponent(typeof(RigidbodyController))]
 public class PlayerController : MonoBehaviour
@@ -8,6 +9,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float MovementSpeed = 5f;
     [SerializeField] private float MovementLerpWeight = 0.15f;
 
+    public enum States { Idle, Walking, Attacking, InKnockback }
+    private States currentState = States.Idle;
 
     private RigidbodyController myRigidbody;
 
@@ -17,26 +20,50 @@ public class PlayerController : MonoBehaviour
         myRigidbody.MovementLerpWeight = MovementLerpWeight;
     }
 
-    bool reset = false;
+
 
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
+        currentState = GetNewState();
+        DoStateLogic();
+        print(currentState);
         //myRigidbody.velocity = Vector2.Lerp(myRigidbody.velocity, new Vector2(x, y).normalized * MovementSpeed, MovementLerpWeight);
-        myRigidbody.velocity = new Vector2(x, y).normalized * MovementSpeed;
-        if(reset)
-            myRigidbody.velocity = Vector2.zero;
 
         if (Input.GetMouseButtonDown(0))
         {
-            print("RESETING");
-            reset = false;
-            MyUtils.Time.SetTimeout(() => {
-                reset = false;
-            }, 0.2f, this);
+            
         }
-
     }
+
+    private States GetNewState()
+    {
+        switch(currentState)
+        {
+            default:
+            case States.Idle:
+                if (myRigidbody.Rigidbody.velocity != Vector2.zero)
+                    return States.Walking;
+                return States.Idle;
+            case States.Walking:
+                if (myRigidbody.Rigidbody.velocity == Vector2.zero)
+                    return States.Idle;
+                return States.Walking;
+        }
+    }
+
+    private void DoStateLogic()
+    {
+        switch (currentState)
+        {
+            default:
+                float x = Input.GetAxisRaw("Horizontal");
+                float y = Input.GetAxisRaw("Vertical");
+                myRigidbody.velocity = new Vector2(x, y).normalized * MovementSpeed;
+                break;
+            
+        }
+    }
+
+
+
 }
