a62c84c1d1fdfafe98d4b2884c25726ed85516a8
fireWizard23 <62689803+fireWizard23@users.noreply.github.com>
2021-07-09 08:27:03 +0800

    Commit 0.172 - Static String class
    Made the static string class to contain the static strings that will be used throughout the project.

--- project/Topdown Shooter ver 2/Assets/Scriptable Objects/MeleeWeaponSO.cs
+++ project/Topdown Shooter ver 2/Assets/Scriptable Objects/MeleeWeaponSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName ="Scriptables/MeleeAsset", fileName ="NewMelee")]
+[CreateAssetMenu(menuName = "Scriptables/MeleeAsset", fileName ="NewMelee")]
 public class MeleeWeaponSO : WeaponSO
 {
     
